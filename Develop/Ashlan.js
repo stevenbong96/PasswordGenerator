e210bb937a34216f258f27eb770c71b422c57474
Steven Bong <stevenseal99@yahoo.com>
2020-09-10 15:07:15 -0700

    Working on the generatePassword function. Put some criteria restrictions for user input. Getting the notes from Ashlan review session at the end of the class

---
+++ Develop/Ashlan.js
@@ -0,0 +1,25 @@
+// HW Review
+
+// Create the generatePassword function that returns the final password and all of my code should live inside this function
+
+// Create a prompt to ask the length of the password and save it to a VAR
+
+// create a confirm to ask the user if they want to lowercase char
+
+// create a confirm to ask the user if they want to uppercase char
+
+// create a confirm to ask the user if they want to numbers
+
+// create a confirm to ask the user if they want to symbols
+
+// After I'm done with the prompts I have to create the password
+
+// Create arrays with all needed char
+
+// Create a var that holds the character that the user wants to have in their password
+
+// Add validations to make sure at least one type of char is selected and alert the user if there are not types being selected
+
+// Add validations to the length to make sure it is between 8-128 char
+
+// 
--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Define the variables
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let length = "";
+// let length = "";
 const number = "1234567890";
 const symbol = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 let choices = "";
@@ -18,24 +18,32 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the password input
 function writePassword() {
-  // var password = generatePassword();
+  // var password = generatePassword(askLower, askUpper, askNumber, askSymbol);
   // var passwordText = document.querySelector("#password");
 
   // passwordText.value = password;
 
   //  Asking user criteria for creating password
-  let question1 = confirm("Do you want to include lowercase?");
-  let question2 = confirm("Do you want to include uppercase?");
-  let question3 = confirm("Do you want to include numbers?");
-  let question4 = confirm("Do you want to include symbols?");
-  var password = generatePassword(question1,  question2, question3, question4);
+  let length = prompt("How long you want to make your password ?");
+    if(length < 8){
+      alert("Please put at least 8 characters")
+    } else if(length > 128){
+      alert("Please put no more than 128 characters")
+    }
+
+  let askLower = confirm("Do you want to include lowercase?");
+  let askUpper = confirm("Do you want to include uppercase?");
+  let askNumber = confirm("Do you want to include numbers?");
+  let askSymbol = confirm("Do you want to include symbols?");
+  if(askLower === false && askUpper === false && askNumber === false && askSymbol === false){
+    alert("Please select at least one validations!")
+  }
+  var password = generatePassword(askLower, askUpper, askNumber, askSymbol);
+
 }
 
 //  New function
-function generatePassword (a, b, c, d){
-  let generatePassword = '';
-  
-  length = prompt("How long you want to make your password ?");
+function generatePassword (a, b, c, d){ 
 
   if(a === true){
     choices = choices.concat(lowerCase);
@@ -54,15 +62,18 @@ function generatePassword (a, b, c, d){
     console.log(choices);
   }
 
-  const wordDam = a + b + c + d;
+  // const wordDam = choices;
 
-  let giantArray = wordDam.split("");
+  let giantArray = choices.split("");
 
   for(i = 0; i < length; i+= wordDam){
     let randomizer = arr[Math.floor(Math.random() * arr.length)];
     let pass = pass +  randomizer;
     return pass;
   }
+
+  let generatePassword = "";
+
   // length = prompt("How long"); continue to line 54
 
   // Create new if statement for numbers and symbols
@@ -77,33 +88,11 @@ function generatePassword (a, b, c, d){
 
   // let pass += randomizer;
   // return pass
-}
-const randomArray = {
-  a: randomLowerCase, b: randomUpperCase, c: randomNumber, d: randomSymbol
-}
 
-function randomLowerCase(){
-  let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lowerCase = letters.toLowerCase();
-  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
-}
-
-function randomUpperCase(){
-  let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const upperCase = letters.toUpperCase();
-  return upperCase[Math.floor(Math.random() * upperCase.length)];
-}
-
-function randomNumber(){
-  const number = "1234567890";
-  return number[Math.floor(Math.random() * number.length)];
-}
-
-function randomSymbol(){
-  const symbol = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  let symbolArray = symbol.split("");
-  symbolArray.push('"');
-  return symbol[Math.floor(Math.random() * symbol.length)];
+  // Create while loop to rerun when user didn't put numbers of length
+  // while(length !== number){
+  //   length = prompt("Please put number of length!")
+  // }
 }
 
 // Add event listener to generate button
@@ -112,3 +101,32 @@ generateBtn.addEventListener("click", writePassword);
 
 // Testing
 // console.log(symbolArray);
+
+// Testing new method
+// const randomArray = {
+//   a: randomLowerCase, b: randomUpperCase, c: randomNumber, d: randomSymbol
+// }
+
+// function randomLowerCase(){
+//   let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   const lowerCase = letters.toLowerCase();
+//   return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+// }
+
+// function randomUpperCase(){
+//   let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   const upperCase = letters.toUpperCase();
+//   return upperCase[Math.floor(Math.random() * upperCase.length)];
+// }
+
+// function randomNumber(){
+//   const number = "1234567890";
+//   return number[Math.floor(Math.random() * number.length)];
+// }
+
+// function randomSymbol(){
+//   const symbol = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//   let symbolArray = symbol.split("");
+//   symbolArray.push('"');
+//   return symbol[Math.floor(Math.random() * symbol.length)];
+// }
